30216eacbacc380af76968d959b68c96ae6e73b5
Rajtripathi01 <83864578+Rajtripathi01@users.noreply.github.com>
2021-10-10 16:05:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var wall1,wall2,wall3,wall4,wall5,wall6,wall7,wall8,wall9,wall10,wall11,wall12,wall13,wall14,wall15,
+wall16,wall17,wall18,wall19,wall20,wall21,wall23,wall24,wall25,wall26
+
+function setup() {
+  createCanvas(800,600);
+  wall1=createSprite(400, 20, 630, 10);
+  wall2=createSprite(20, 300, 10, 500);
+  wall3=createSprite(370, 550, 700, 10);
+  wall4=createSprite(710, 250, 10, 450);
+  wall5=createSprite(630, 430, 170, 10);
+  wall6=createSprite(660, 250, 100, 10);
+  wall7=createSprite(610, 300, 10, 100);
+  wall8=createSprite(500, 250, 10, 170);
+  wall9=createSprite(405, 340, 200, 10);
+  wall10=createSprite(490, 170, 210, 10);
+  wall11=createSprite(395, 210, 10, 80);
+  wall12=createSprite(300, 295, 10, 100);
+  wall13=createSprite(270, 240, 80, 10);
+  wall14=createSprite(230, 200, 10, 80);
+  wall14=createSprite(300, 70, 10, 100);
+  wall15=createSprite(370, 380, 10, 90);
+  wall16=createSprite(315, 430, 120, 10);
+  wall17=createSprite(250, 395, 10, 80);
+  wall18=createSprite(300, 490, 10, 120);
+  wall19=createSprite(180, 500, 10,100);
+  wall20=createSprite(660, 100, 100,10);
+  wall21=createSprite(150, 70, 10,100);
+  wall22=createSprite(150, 125, 90,10);
+  wall23=createSprite(100, 190, 10,140);
+  wall24=createSprite(120, 260, 50,10);
+  wall25=createSprite(150, 290, 10,70);
+  wall26=createSprite(76, 405, 100,10);
+
+
+
+
+
+  
+
+}
+
+function draw() {
+  background("green");  
+  drawSprites();
+}
